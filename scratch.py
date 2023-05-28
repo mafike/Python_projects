16fbd5a98f7d64a58f0e68aa386d469d6217ce1a
mafike <mafgenes1@icloud.com>
2023-05-27 18:13:03 -0700

    c3

--- scratch.py
+++ scratch.py
@@ -1,8 +1,6 @@
 # Collections: Counter, namedtuple, orderedDict, defaultdict, deque
 # Itertools: product, permutations, combinations, accumalate, groupby, and infinite itereations
-from itertools import product
-a = [1,2]
-b = [3]
-prod = product(a,b, repeat=2)
-print(list(prod))
-
+from itertools import permutations
+a=[1,2,3]
+perm = permutations(a, 2)
+print(list(perm))
