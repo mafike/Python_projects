24045fb2f5d604acec7b05000070db07e1a33771
mafike <mafgenes1@icloud.com>
2023-06-02 01:39:25 -0700

    b1

---
+++ ex49.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+def div(a,b):
+    return a/b
+result= div(a,b)
+print(f"{a}//{b} = {int(result)}")
+print(f"{a}/{b} = {float(result)}")
+
