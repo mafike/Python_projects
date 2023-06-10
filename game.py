9c3b2967d540913bfd00030ded9a26732fcc0b3d
mafike <mafgenes1@icloud.com>
2023-06-10 00:53:49 -0700

    v5

--- game.py
+++ game.py
@@ -38,8 +38,48 @@ class Death(Scene):
 
 class CentralCorridor(Scene):
     def enter(Self):
-        pass
+        print(dedent("""
+              The Gothons of Planet Percal #25 have invaded your ship and 
+              destroyed your entire crew. You are the last surviving 
+              member and your last mission is to get the neutron destruct
+              bomb from the Weapons Armory, put it in the bridge, and
+              blow the ship up after getting into the escape pod.
 
+              You're running down rhe central corridor to the Weapons 
+              Armory when a Gothon jumps out, red scaly skin, dark grimy 
+              teeth, and evil clown costume flowing around his hate 
+              filled body. He's blocking the door to the Armory and 
+              about to pull a weapon to blast you.
+              """))
+        action = input("> ")
+
+        if action == "shoot!":
+            print(dedent("""
+                  Quick on the draw you yank out your blaster and fire 
+                  it at the Gothon. His clown costume is flowing and 
+                  moving around his body, which throws off your aim.
+                  Your laser hits his costume but misses him entirely.
+                  This completely ruins his brand new costume his mother
+                  bought him, which makes him fly unto an insane rage
+                  and blast you repeatedly in the face until you are 
+                  dead. Then he eats you.
+                  """))
+            return 'death'
+        
+        elif action == "tell a joke":
+            print(dedent("""
+                Lucky for you they made you learn the Gothon insults in 
+                the academy. You tell the one Gothon joke you know:
+                Lbhe Zjalk ahkdh kakd lal, kadopa naldkm ljda,
+                khadh  dak kajd iaid. The Gothon stops, tries 
+                not to laugh, then bursts out laughing and can't move.
+                While he's laughing you run up and shoot him square in 
+                the head putting him down, then jump through the 
+                Weapon Armory door
+                """))
+        else:
+            print("DOES NOT COMPUTE!")
+            return 'central_corridor'    
 class laserWeaponArmory(Scene):
     def enter(self):
         pass
