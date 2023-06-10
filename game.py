12a3d4088bdfb1b311a5d701fa2bfc1090bdce5c
mafike <mafgenes1@icloud.com>
2023-06-10 01:48:08 -0700

    v8

--- game.py
+++ game.py
@@ -164,8 +164,39 @@ class TheBridge(Scene):
             return "the_bridge"
 class EscapePod(Scene):
     def enter(self):
-        pass
+        print(dedent("""
+             You rush though the ship desperately trying to make it to 
+             the escape pod before the whole ship explodes. It seems
+             like hardly any Gothons are on the ship, so your run is 
+             clear interferance. You get to the chamber with the 
+             escape pods, and now need to pick one to make. Some of 
+             them could be damaged but you don't have time to look.
+             There's 5 pods, which one do you take?
+             """))
+        
+        good_pod = randint(1,5)
+        guess = input("[pod #]> ")
 
+        if int(guess) != good_pod:
+            print(dedent("""
+                   You jump into the escape pod {guess} and hit the eject button.
+                   The pod escapes out into the void of space, then 
+                   implodes as the hull ruptures, crushing your body into 
+                   jam jelly.
+                   """))
+            return 'death'
+        
+        else:
+            print(dedent("""
+                   You jump into pod {guess} and hit the eject button.
+                   The pod easily slides out into space heading to the
+                   planet below. As it flies to the palnet, you look
+                   back and see your ship implode then explode like a 
+                   bright star, taking out the Gothon ship at the same
+                   time. You won!
+                   """))
+            return 'finished'
+        
 class Map(object):
     def __init__(self, start_scene):
         pass
