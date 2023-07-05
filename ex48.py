f74272c32b66544ad1109219f09915e375763603
mafike <mafgenes1@icloud.com>
2023-07-05 08:57:11 +0100

    a1

--- ex48.py
+++ ex48.py
@@ -1,15 +1,45 @@
-import math
-import os
-import random
-import re
-import sys
+from sys import exit
+from random import randint
 
-if __name__ == '__main__':
-    n = int(input().strip())
-def check(n):
-        if n % 2 ==0:
-            return "Not Weird"
-        else:
-            return "Weird"
-print(check(n))          
+
+class Scene(object):
+    def enter(self):
+        pass
+
+class Engine(object):
+    def __init__(self, scene_name):
+        pass
+    def play(self):
+        pass
+
+class Central_Corridor(Scene):
+    def enter(self):
+        pass
+
+class Death(Scene):
+    def enter(self):
+        pass
+
+class Lazer_Weapon_Armory(Scene):
+    def enter(self):
+        pass
+
+class Bride(Scene):
+    def enter(self):
+        pass
+
+class Escape_Pod(Scene):
+    def enter(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
 
