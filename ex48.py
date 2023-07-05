7e1b83b4f9f0741e02758534f2993f065e48bd34
mafike <mafgenes1@icloud.com>
2023-07-05 11:29:25 +0100

    c2

---
+++ ex48.py
@@ -0,0 +1,15 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+def check(n):
+        if n % 2 ==0:
+            return "Not Weird"
+        else:
+            return "Weird"
+print(check(n))          
+
